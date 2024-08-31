8a934568a7bd6cb10f6eaab5dbb86c3452f113fd
monayn <m.yousefinejad78@gmail.com>
2024-09-01 00:12:15 +0330

    reverse is done

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,10 @@
 // write your func here
+const str = "this is going to be reverse";
+
+function reverseStr(str) {
+  let splitString = str.split("");
+  let reverseArr = splitString.reverse();
+  let joinArr = reverseArr.join("");
+  console.log(joinArr);
+}
+reverseStr(str);
