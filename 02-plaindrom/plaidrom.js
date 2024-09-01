f6a2384859053a8c571877a48a0ead859e017b6e
monayn <m.yousefinejad78@gmail.com>
2024-09-01 20:30:26 +0330

    palindrom is done

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,11 @@
 // write your func here
+
+function palindrome(text) {
+  if (text[0] === text[text.length - 1]) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+palindrome("songs");
