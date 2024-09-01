7860e7b0aa568c94a936e3b9edca4a613128254a
monayn <m.yousefinejad78@gmail.com>
2024-09-01 20:54:51 +0330

    factoriel is here

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,14 @@
 // write your func here
+
+// let n = 4;
+function factoriel(a) {
+  let num = 1;
+  if (a === 0) {
+    return 1;
+  }
+  for (let i = 2; i <= a; i++) {
+    num = num * i;
+  }
+  return num;
+}
+console.log(factoriel(12));
