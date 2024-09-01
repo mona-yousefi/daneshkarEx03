0a82f26a9202830f46906dd8f898e644483eba66
monayn <m.yousefinejad78@gmail.com>
2024-09-01 22:47:36 +0330

    I found the max of an array

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,11 @@
 // write your func here
+
+const myArr = [12, 45, 10, 27, 1045];
+let max = myArr[0];
+
+for (let i = 1; i < myArr.length; i++) {
+  if (myArr[i] > max) {
+    max = myArr[i];
+  }
+}
+console.log(max);
