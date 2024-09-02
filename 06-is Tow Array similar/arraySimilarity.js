cc9dc617299eabda6b3812f1d0e005bdaff3f3bc
monayn <m.yousefinejad78@gmail.com>
2024-09-02 12:17:07 +0330

    checking two array is similar

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,19 @@
 // write your func here
+
+const array1 = [1, 4, 6, 3, 7];
+const array2 = [1, 4, 6, 3, 7];
+
+function compareArrays(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log(compareArrays(array1, array2));
