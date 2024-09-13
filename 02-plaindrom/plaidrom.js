702811413d954d8953ad9a456f9c99aa04cb2af2
monayn <m.yousefinejad78@gmail.com>
2024-09-13 11:43:28 +0330

    updated palindrome

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,11 +1,11 @@
 // write your func here
 
-function palindrome(text) {
-  if (text[0] === text[text.length - 1]) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
+function reverse(str) {
+  return str.split("").reverse().join("");
 }
 
-palindrome("songs");
+function plaindrom(str) {
+  return str === reverse(str);
+}
+console.log(plaindrom("racecar"));
+console.log(plaindrom("song"));
